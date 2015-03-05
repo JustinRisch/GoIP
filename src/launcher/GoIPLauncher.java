1ebb080f1b8efe211b1cfd3c8b75f64aeceda841
Justin Risch <JustinRisch@gmail.com>
2015-03-05 09:53:48 -0500

    version 1.3.1

--- src/launcher/GoIPLauncher.java
+++ src/launcher/GoIPLauncher.java
@@ -30,7 +30,7 @@ public final class GoIPLauncher {
 
 	// version members
 	private static String currentVersion;
-	private static final String version = currentVersion = "1.3.0";
+	private static final String version = currentVersion = "1.3.1";
 
 	final public static void main(String[] args) {
 
